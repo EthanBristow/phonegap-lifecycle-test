65b569d3b4b0571b59d15d66a7894de8bf1d1d3b
EthanBristow <brie1_15@uni.worc.ac.uk>
2016-10-18 10:38:41 +0100

    Storing and Retrieving Data

--- www/js/index.js
+++ www/js/index.js
@@ -1,42 +1,31 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+var key = "pen";
+var value = "blue";
+
+var key1 = "phone";
+var value1 = "green";
+
+var key2= "cable";
+var value2= 15;
+
+window.localStorage.setItem( key, value );
+window.localStorage.setItem( key1, value1 );
+window.localStorage.setItem( key2, value2 );
+
+var value2 = window.localStorage.getItem(key2);
 
     function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false); //Event listener for device ready life cycle event and running the onDeviceReady function
+        //document.addEventListener("deviceready", onDeviceReady, false); //Event listener for device ready life cycle event and running the onDeviceReady function
 		console.log("device ready");
+        
+        $("#item").text(value2);
+        $("#length").text(window.localStorage.length);
     }
 	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
-    //This function above is where jQueryMobile is used. Used to increment  the life cycle counts.
-
-
-    // device APIs are available
-    //
+	
     function onDeviceReady() { //This function runs when event listener for deviceready is true
 		alert("device ready");
         
 		document.addEventListener("resume", onResume, false); //Event listener added for when resume life cycle event is true and then runs the onResume function
 		document.addEventListener("pause", onPause, false);//Event listener added for when pause life cycle event is true and then runs the onPause function
-		
-		launched_count++;
-		updateDisplay();
     }
 
-    // Handle the pause event
-    //
-    function onPause() { //This function runs when event listener for onPause is true
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() { //This function runs when event listener for onResume is true
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
