01675a24fae8cc6b2aa06c1f491d1e19430adab0
EthanBristow <brie1_15@uni.worc.ac.uk>
2016-10-18 11:39:41 +0100

    Added JSON

--- www/js/index.js
+++ www/js/index.js
@@ -4,21 +4,37 @@ var value = "blue";
 var key1 = "phone";
 var value1 = "green";
 
-var key2= "cable";
+var key2 = "cable";
 var value2= 15;
+
+//JSON Object
+var chocbar = {
+    "Energy": "2280",
+    "Fat": "32.4",
+    "Carbohydrate": "56.0",
+    "Protein": "6.7",
+    "Salt": "0.25"
+}
+
+var chocbarString = JSON.stringify(chocbar);
+window.localStorage.setItem("chocbar", chocbarString);
 
 window.localStorage.setItem( key, value );
 window.localStorage.setItem( key1, value1 );
 window.localStorage.setItem( key2, value2 );
 
-var value2 = window.localStorage.getItem(key2);
+var result = window.localStorage.getItem("cable");
 
     function onLoad() {
         //document.addEventListener("deviceready", onDeviceReady, false); //Event listener for device ready life cycle event and running the onDeviceReady function
 		console.log("device ready");
         
-        $("#item").text(value2);
+        $("#item").text(result);
         $("#length").text(window.localStorage.length);
+        var getChocbar = window.localStorage.getItem("chocbar");
+        var chocbarJSON = JSON.parse(getChocbar);
+        $("#JSON").text("Energy: " + chocbarJSON.Energy + " Fat: " + chocbarJSON.Fat + " Carbohydrate: " + chocbarJSON.Carbohydrate + " Protein: " + chocbarJSON.Protein + " Salt: " + chocbarJSON.Salt);
+        
     }
 	
 	
